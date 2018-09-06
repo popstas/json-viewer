1ca7db35b3dd0aa7bb156fb0d76bcc0f88b6125e
Stanislav Popov <popstas@gmail.com>
2018-09-06 23:13:40 +0500

    feat: формат запросов json-query

--- store/index.js
+++ store/index.js
@@ -1,4 +1,5 @@
 import pjson from '~/package.json';
+import jsonQuery from 'json-query';
 import dateformat from 'dateformat';
 
 export const state = () => ({
@@ -47,22 +48,64 @@ export const mutations = {
 };
 
 export const actions = {
-  // site_info.engine:bitrix cron:0
+  // фильтрует sites на основе filter
   filterSites({ commit, state }, payload) {
     const q = state.filter.q.toLowerCase();
-    let filters = q.split(' ');
-    let filteredSites = state.sites;
+    let filteredSites = [];
 
-    for (let fid in filters) {
-      const [name, value] = filters[fid].split(':');
+    // empty query
+    if (q == '') filteredSites = state.sites;
 
-      filteredSites = filteredSites.filter(site => {
-        const [parent, child] = name.split('.');
-        if (child) return site[parent] && site[parent][child] == value;
-        else return site[name] == value;
-        // console.log('site: ', site.domain, site[name], site[name] == value);
-      });
+    // json-query
+    // регулярка отсекает значения, которые могут быть именами полей
+    // запросы с ':' фильтруются, т.к. json-query выдает фигню и для совместимости с custom format
+    if (filteredSites.length == 0 && !q.match(/^[a-z0-9_]+$/) && q.indexOf(':') === -1) {
+      try {
+        const res = jsonQuery('data[*' + q + ']', {
+          data: { data: state.sites },
+          allowRegexp: true
+        });
+        filteredSites = res.value;
+
+        // normalize to array
+        if (!filteredSites) filteredSites = [];
+        /* if (typeof filteredSites === 'object' && !obj instanceof Array){ // for single result
+          filteredSites = [filteredSites];
+        } */
+      } catch (e) {
+        // console.log(`failed to filter json-query data${q}`, e);
+      }
+      // console.log('filteredSites after json-query: ', filteredSites);
     }
+
+    // custom format:
+    // site_info.engine:bitrix cron:0
+    if (filteredSites.length == 0) {
+      // console.log('custom search');
+      let filters = q.split(' ');
+      filteredSites = state.sites;
+      let errors = 0;
+      for (let fid in filters) {
+        const [name, value] = filters[fid].split(':');
+        if (!value) {
+          errors++;
+          // console.log('error: ', filters[fid] + ' not valid filter');
+          break;
+        }
+
+        const [parent, child] = name.split('.');
+
+        filteredSites = filteredSites.filter(site => {
+          if (child) return site[parent] && site[parent][child] == value;
+          else return site[name] == value;
+          // console.log('site: ', site.domain, site[name], site[name] == value);
+        });
+        // console.log('filteredSites after ' + filters[fid] + ': ', filteredSites);
+      }
+
+      if (errors > 0) filteredSites = [];
+    }
+
     commit('filteredSites', filteredSites);
   }
 };
