bf5d2ad2b2c3381464064484e550cbdcced1daa3
Stanislav Popov <popstas@gmail.com>
2018-11-15 23:06:30 +0500

    refactor: scss, пресеты колонок и фильтров в отдельных файлах

---
+++ assets/js/presets/columns.conf.js
@@ -0,0 +1,74 @@
+module.exports = {
+  default: {
+    name: 'default',
+    columns: [
+      'domain_idn',
+      'host',
+      'prod',
+      'engine',
+      'meta_year',
+      'meta_visitors',
+      'yandex_tcy',
+      'files_size',
+      'errors'
+    ]
+  },
+  cron: {
+    name: 'cron',
+    columns: ['domain_idn', 'host', 'prod', 'cron']
+  },
+  bitrix: {
+    name: 'bitrix',
+    columns: [
+      'domain_idn',
+      'host',
+      'prod',
+      'email',
+      'engine_version',
+      'bitrix_image_quality',
+      'bitrix_total_mark_value',
+      'files_count',
+      'files_size',
+      'git_clean',
+      'yandex_tcy',
+      'h1_count'
+    ]
+  },
+  pagespeed_performance: {
+    name: 'pagespeed_performance',
+    columns: [
+      'domain_idn',
+      'host',
+      'prod',
+      'engine',
+      'errors',
+      'lighthouse_performance',
+      'lighthouse_interactive',
+      'lighthouse_speed_index',
+      'lighthouse_first_contentful_paint',
+      'lighthouse_first_cpu_idle',
+      'lighthouse_first_meaningful_paint',
+      'lighthouse_input_latency'
+    ]
+  },
+  pagespeed_score: {
+    name: 'pagespeed_score',
+    columns: [
+      'domain_idn',
+      'host',
+      'prod',
+      'engine',
+      'errors',
+      'lighthouse_all',
+      'lighthouse_performance',
+      'lighthouse_pwa',
+      'lighthouse_accessibility',
+      'lighthouse_best_practices',
+      'lighthouse_seo'
+    ]
+  },
+  https: {
+    name: 'https',
+    columns: ['domain_idn', 'https', 'email']
+  }
+};
---
+++ assets/js/presets/filters.conf.js
@@ -0,0 +1,46 @@
+module.exports = [
+  {
+    name: 'prod',
+    q: 'prod=1'
+  },
+  {
+    name: 'bitrix',
+    q: 'engine=bitrix&prod=1'
+  },
+  {
+    name: 'drupal',
+    q: 'engine=drupal&prod=1'
+  },
+  {
+    name: 'without cron',
+    q: 'engine=drupal&prod=1&cron=0'
+  },
+  {
+    name: 'without git',
+    q: 'prod=1&git=0'
+  },
+  {
+    name: 'dirty git',
+    q: 'prod=1&git=1&git_clean=0'
+  },
+  {
+    name: 'h1 > 1',
+    q: 'h1_count>1&prod=1'
+  },
+  {
+    name: 'without https',
+    q: 'https=0&prod=1'
+  },
+  {
+    name: 'drupal without cache',
+    q: 'engine=drupal&prod=1&cache=0&boost=0'
+  },
+  {
+    name: 'drupal without js min',
+    q: 'engine=drupal&prod=1&preprocess_js=0'
+  },
+  {
+    name: 'yandex_maps',
+    q: 'yandex_maps=1'
+  }
+];
