103f1b7751bbab39db0af7b6a21d7614d71d95b6
Stanislav Popov <popstas@gmail.com>
2018-12-09 00:50:51 +0500

    feat: индикация клика по чекбоксу колонки

--- components/ColumnField.vue
+++ components/ColumnField.vue
@@ -1,15 +1,27 @@
 <template>
-  <div class="available-fields__field"
+  <div class="available-fields__field" ref="component"
     :title="field.name + (field.comment ? ` \n${field.comment}` : '') + (field.command ? ` \n${field.command}` : '')"
-    @click="$emit('click', field)"
+    @click="click"
   >
     <input type="checkbox" :checked="checked">
     <label>{{ field.comment || field.title }}</label>
   </div>
 </template>
 
+<style lang="scss">
+.available-fields__field{
+  &.clicked { background: #ffffaa;}
+}
+</style>
+
 <script>
 export default {
-  props: ['field', 'checked']
+  props: ['field', 'checked'],
+  methods: {
+    click() {
+      this.$refs.component.classList.add('clicked');
+      this.$emit('click', this.field);
+    }
+  }
 }
 </script>
