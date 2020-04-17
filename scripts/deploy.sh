fb185f862968178df3e61d9b9b6b84787dbf16e7
Stanislav Popov <popstas@gmail.com>
2020-04-18 00:32:12 +0500

    chore: deploy on npm version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -eu
 
-cd /home/popstas/projects/js/viasite-projects-list/
-git pull
-npm i
-npm run generate
-rsync -r dist/ viasite@dev.viasite.ru:/home/viasite/www/dev.viasite.ru/viasite-projects/
+ssh popstas@popstas-server "cd /home/popstas/projects/js/viasite-projects-list/ && git pull && npm i && npm run generate && rsync -r dist/ viasite@dev.viasite.ru:/home/viasite/www/dev.viasite.ru/viasite-projects/"
