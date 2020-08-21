93edc604e72a8d1ed02637d906c3077c1c48e99c
Stanislav Popov <popstas@gmail.com>
2020-08-21 16:51:53 +0500

    chore: change deploy to viasite/site-audit-seo-viewer:gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ cd dist
 git init
 git add -A
 git commit -m "deploy"
-git remote add origin git@github.com:viasite/site-audit-seo.git
+git remote add origin git@github.com:viasite/site-audit-seo-viewer.git
 git push --force origin master:gh-pages
 cd ..
